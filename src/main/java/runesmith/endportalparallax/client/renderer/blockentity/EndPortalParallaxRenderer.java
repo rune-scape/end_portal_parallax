b3c2437f5a490457e8b98af9a83e4cac1c952a5b
rune-scape <allie.smith.epic@gmail.com>
2022-10-29 17:45:55 -0700

    Initial commit

---
+++ src/main/java/runesmith/endportalparallax/client/renderer/blockentity/EndPortalParallaxRenderer.java
@@ -0,0 +1,29 @@
+package runesmith.endportalparallax.client.renderer.blockentity;
+
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.blockentity.TheEndPortalRenderer;
+import net.minecraft.world.level.block.entity.TheEndPortalBlockEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.NotNull;
+import runesmith.endportalparallax.client.renderer.Renderer;
+
+@OnlyIn(Dist.CLIENT)
+public class EndPortalParallaxRenderer<T extends TheEndPortalBlockEntity> extends TheEndPortalRenderer<T> {
+    public EndPortalParallaxRenderer(BlockEntityRendererProvider.Context context) {
+        super(context);
+    }
+
+    @Override
+    public void render(@NotNull T blockEntity, float partialTicks, PoseStack poseStack, MultiBufferSource bufferSource, int combinedLightIn, int combinedOverlayIn) {
+        Renderer.renderPortal(blockEntity, poseStack.last().pose(), (BufferBuilder) bufferSource.getBuffer(renderType()), getOffsetUp(), getOffsetDown());
+    }
+
+    protected @NotNull RenderType renderType() {
+        return Renderer.getRendertypeEndPortalParallax();
+    }
+}
